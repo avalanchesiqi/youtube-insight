a9fe88bf0e3a1a827a537c13e050b1108dc714d9
avalanchesiqi <wusiqi.china@gmail.com>
2018-03-05 10:06:08 +1100

    add a filter on recency

--- util_py/get_video_recency_availability.py
+++ util_py/get_video_recency_availability.py
@@ -1,38 +1,48 @@
 """
 Detect availability of a list of videos on webpage, this includes existence of video and historical attention series.
+We add an additional filter to select videos that published after 1 Jan, 2017.
 """
 
 import argparse, requests, logging
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 
-def is_available(video_id):
+def is_recent_available(video_id):
     video_page = 'https://www.youtube.com/watch?v={0}'.format(video_id)
     try:
         r = requests.get(video_page)
         soup = BeautifulSoup(r.text, 'lxml')
-        for s in soup.find_all('span', {'class': 'yt-ui-menu-item-label'}):
-            if not (s and s.string):
-                continue
-            if s.string == 'Statistics':
-                return True
+        d = soup.find('div', {'id': 'watch-uploader-info'})
+        # filter 1 on recency: must publish after 2017
+        if datetime.strptime(d.text[13:], '%b %d, %Y') > datetime.strptime('Dec 31, 2016', '%b %d, %Y'):
+            for s in soup.find_all('span', {'class': 'yt-ui-menu-item-label'}):
+                if not (s and s.string):
+                    continue
+                # filter 2 on availability: must contain historical attention series
+                if s.string == 'Statistics':
+                    return True
     except Exception as e:
         logging.ERROR('{0} with msg {1}'.format(video_id, str(e)))
     return False
 
 
-def test_availability():
-    # an available video: Adele - Hello
-    assert is_available('YQHsXMglC9A') is True
-    # an unavailable video: How To Service Cartridge Bearings On Your Road Bike
-    assert is_available('nxBWBT2lmR8') is False
+def test_recent_available():
+    # a recent and statistics available video: EXO 'Power' MV
+    assert is_recent_available('sGRv8ZBLuW0') is True
+    # a recent but statistics unavailable video: How To Service Cartridge Bearings On Your Road Bike
+    assert is_recent_available('nxBWBT2lmR8') is False
+    # a non-recent and statistics available video: Adele - Hello
+    assert is_recent_available('YQHsXMglC9A') is False
+    # a non-recent and statistics unavailable video: Road Bike Party 2 - Martyn Ashton
+    assert is_recent_available('HhabgvIIXik') is False
 
 
 def start_query(input_path, output_data, verbose=False):
     with open(input_path, 'r') as fin:
         for line in fin:
             vid = line.rstrip()
-            if is_available(vid):
+            if is_recent_available(vid):
                 output_data.write('{0}\n'.format(vid))
                 if verbose:
                     print(vid, True)
